fa565931c8b6b9e605a2158696503e1050d7297e
guikage <guieinloft@protonmail.com>
2023-04-28 00:26:48 -0300

    modificando calculadora fodona

--- calc3.c
+++ calc3.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 float opera(float a, float b, char op){
     switch (op){
@@ -13,40 +11,41 @@ float opera(float a, float b, char op){
         case '/':
             return a/b;
         default:
-            return a+b;
+            return a;
     }
 }
 
 int main(){
     float total = 0.0;
-    float resto = 1.0;
+    float resto = 0.0;
     float valor = 0.0;
-    char op;
-    char op2;
+    char op = ' ';
+    char op2 = '+';
 
     scanf("%f", &total);
     op = getchar();
-    if(op == '*' || op == '/'){
+    while (op == ' ') op = getchar(); //enquanto receber ' ' repete
+    if(op == '*' || op == '/'){ //se for mult ou div passa total pro resto pra efetuar op
         resto = total;
         total = 0;
-    } else {
-        resto = 0;
+    } else { //se for adc ou sub salva a op como op2
         op2 = op;
     }
     for(;;){
-        if (op == '\n'){
-            total = opera(total, resto, op2);
+        if (op == '\n' || op == '='){ //para o codigo
+            total = opera(total, resto, op2); //confere se tem resto no resto
             break;
         }
         scanf("%f", &valor);
         if (op == '*' || op == '/'){
-            resto = opera(resto, valor, op);
+            resto = opera(resto, valor, op); //opera o resto com o valor
         } else if (op == '+' || op == '-'){
-            total = opera(total, resto, op2);
-            resto = valor;
-            op2 = op;
+            total = opera(total, resto, op2); //opera o total com o resto usando op2
+            resto = valor; //valor recebido se torna o resto
+            op2 = op; //op recebido se torna op2
         }
         op = getchar();
+        while (op == ' ') op = getchar();
     }
 
     printf("RESULTADO: %.2f\n", total);
--- l1_t2_guilherme_einloft.c
+++ l1_t2_guilherme_einloft.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 float opera(float a, float b, char op){
     switch (op){
