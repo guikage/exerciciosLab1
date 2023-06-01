51df596ea843d712feef0029cf9e30582c10c804
guikage <guieinloft@protonmail.com>
2023-05-31 23:43:14 -0300

    modificando colors 3

--- colors_3.c
+++ colors_3.c
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include "tela.h"
 
-#define TMAX 20.0
+#define TMAX 30.0
 
 typedef struct{
     int r;
@@ -24,15 +24,19 @@ typedef struct{
     double tempo;
 } estado;
 
-void barra(int lin, int col, char cor, int valor){
+const cor RED = {1, 0, 0};
+const cor GREEN = {0, 1, 0};
+const cor BLUE = {0, 0, 1};
+
+void barra(int lin, int col, cor cor, int valor){
     tela_lincol(lin, col);
-    int r = 0, g = 0, b = 0;
-    if(cor == 'r') r = 10;
-    if(cor == 'g') g = 10;
-    if(cor == 'b') b = 10;
     for(int i = 0; i < 26; i++){
-        if (valor/10 == i) tela_cor_fundo(255, 255, 255); //branco
-        else tela_cor_fundo(i*r, i*g, i*b); //cor escolhida
+        if (valor/10 == i) {
+            tela_cor_fundo(255, 255, 255); //branco
+        }
+        else {
+            tela_cor_fundo(i*cor.r*10, i*cor.g*10, i*cor.b*10); //cor escolhida
+        }
         putchar(' ');
     }
     tela_cor_normal();
@@ -121,28 +125,71 @@ float pontuacao(cor crand, cor cuser, double tempo){
     pontos_g = pontuacao_cor(crand.g, cuser.g);
     pontos_b = pontuacao_cor(crand.b, cuser.b);
     pontos = 100 - (pontos_r + pontos_g + pontos_b)/3;
-    pontos += pontos / 0.05 * (TMAX - tempo);
+    pontos += pontos * 0.05 * (TMAX - tempo);
     return pontos;
 }
 
-void le_arquivo(int vetor[5], char nomes[5][4]){
+int gerencia_placar(float pontos, float placar[5], char nomes[5][4]){
+    for (int i = 0; i < 5; i++){
+        if(pontos > placar[i]){
+            for (int j = 4; j > i; j--){
+                placar[j] = placar[j-1];
+                strcpy(nomes[j], nomes[j-1]);
+            }
+            placar[i] = pontos;
+            return (i+1);
+        }
+    }
+    return 0;
+}
+
+void pega_nome(char nomes[5][4], int pos, int lin, int col){
+    int c, i = 0;
+    bool enter = false;
+    while(i < 4){
+        if(!enter){
+            c = tela_le_char();
+            if (c == c_enter){
+                enter = true;
+            }
+            else if (c == c_back){
+                i = 0;
+                tela_lincol(lin, col);
+                printf("___");
+                tela_lincol(lin, col);
+            }
+            else if (c > 32 && c < 127){
+                nomes[pos][i] = c;
+                printf("%c", c);
+                i++;
+            }
+        }
+        else{
+            nomes[pos][i] = '\0';
+            i++;
+        }
+    }
+    nomes[pos][3] = '\0';
+}
+
+void le_arquivo(float vetor[5], char nomes[5][4]){
     FILE *arq = fopen("./placar.txt", "r");
     if (arq == NULL){
 	    arq = fopen("./placar.txt", "w");
-	    fprintf(arq, "AAA 0\nAAA 0\nAAA 0\nAAA 0\nAAA 0");
+	    fprintf(arq, "AAA -1\nAAA -1\nAAA -1\nAAA -1\nAAA -1");
 	    fclose(arq);
 	    arq = fopen("./placar.txt", "r");
     }
     for (int i = 0; i < 5; i++){
-        fscanf(arq, "%s %d", nomes[i], &vetor[i]);
+        fscanf(arq, "%s %f", nomes[i], &vetor[i]);
     }
     fclose(arq);
 }
 
-void escreve_arquivo(int vetor[5], char nomes[5][4]){
+void grava_arquivo(float vetor[5], char nomes[5][4]){
     FILE *arq = fopen("./placar.txt", "w");
     for(int i = 0; i < 5; i++){
-	fprintf(arq, "%s %d\n", nomes[i], vetor[i]);
+	fprintf(arq, "%s %.1f\n", nomes[i], vetor[i]);
     }
     fclose(arq);
 }
@@ -162,7 +209,10 @@ void inicializa(estado *e){
 void le_tecla(estado *e){
     int tecla;
     tecla = tela_le_char();
-    if(tecla == c_up || tecla == c_down){
+    if (tecla == c_enter){
+        e->fim = true;
+    }
+    else if(tecla == c_up || tecla == c_down){
         muda_cor(&(e->cor), tecla);
     }
     else{
@@ -193,17 +243,36 @@ void mostra_tela(estado e){
     quadrado(1, 1, e.crand);
     quadrado(7, 1, e.cuser);
     indica_cor(7, 7, e.cor);
-    barra(7, 8, 'r', e.cuser.r);
-    barra(9, 8, 'g', e.cuser.g);
-    barra(11, 8, 'b', e.cuser.b);
+    barra(7, 8, RED, e.cuser.r);
+    barra(9, 8, GREEN, e.cuser.g);
+    barra(11, 8, BLUE, e.cuser.b);
     tela_lincol(1, 7);
     printf("PONTUAÇÃO:\t%03.1f", e.pontos);
     tela_lincol(2, 7);
-    printf("TEMPO:\t%03.1lf", e.tempo);
+    printf("TEMPO:\t\t%03.1lf", e.tempo);
+}
+
+void imprime_cores_finais(int lin, int col, cor cor, bool user){
+    tela_lincol(lin, col);
+    if (user) {
+        printf("COR DO USUARIO:");
+    }
+    else {
+        printf("COR GERADA:");
+    }
+    quadrado(lin+2, col, cor);
+    barra(lin+2, col+6, RED, cor.r);
+    barra(lin+4, col+6, GREEN, cor.g);
+    barra(lin+6, col+6, BLUE, cor.b);
 }
 
 void termina(estado *e){
+    tela_limpa();
     e->pontos = pontuacao(e->crand, e->cuser, e->tempo);
+    imprime_cores_finais(1, 1, e->crand, false);
+    imprime_cores_finais(9, 1, e->cuser, true);
+    tela_lincol(17,1);
+    printf("PONTUACAO:\t%03.1f\nTEMPO:\t\t%03.1lf", e->pontos, e->tempo);
 }
 
 void partida(estado *e){
@@ -215,12 +284,52 @@ void partida(estado *e){
     termina(e);
 }
 
+void gera_posicao(estado partida, float placar[5], char nomes[5][4]){
+    int posicao = gerencia_placar(partida.pontos, placar, nomes);
+    if (posicao != 0){
+        tela_lincol(20, 1);
+        printf("VOCE FICOU EM %do LUGAR\nDIGITE SEU NOME: ___", posicao);
+        tela_lincol(21, 18);
+        pega_nome(nomes, posicao-1, 21, 18);
+        grava_arquivo(placar, nomes);
+    }
+}
+
+void mostra_placar(float placar[5], char nomes[5][4]){
+    tela_lincol(0, 0);
+    printf("MELHORES PONTUACOES: \n\n");
+    for (int i = 0; i < 5; i++){
+        if(placar[i] >= 0){
+            printf("%3s: %03.1f\n", nomes[i], placar[i]);
+        }
+    }
+    int c;
+    printf("\n\nPRESSIONE ENTER PARA SAIR");
+    do{
+        c = tela_le_char();
+    } while(c != c_enter);
+}
+
 int main(){
     srand(time(0));
     tela_cria();
     tela_mostra_cursor(false);
     estado e;
-    partida(&e);
+    float placar[5];
+    char nomes[5][4];
+    int continua = -1000;
+    le_arquivo(placar, nomes);
+    do{
+        partida(&e);
+        gera_posicao(e, placar, nomes);
+        tela_lincol(23, 1);
+        printf("PRESSIONE ENTER PARA JOGAR NOVAMENTE\n(qualquer outro botao sai)");
+        do{
+            continua = tela_le_char();
+        } while(continua == c_none);
+        tela_limpa();
+    } while(continua == c_enter);
+    mostra_placar(placar, nomes);
     tela_destroi();
     return 0;
 }
